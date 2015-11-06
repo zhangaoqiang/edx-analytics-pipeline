4330e2ab3b57c0f540d1f4449f371ce024a40e07
Umer Farooq <umerfarooq@UmerFarooq-MBP.local>
2015-11-06 15:13:59 +0500

    Event count per date functionality added and task registered in setup.cfg

--- edx/analytics/tasks/event_type_dist.py
+++ edx/analytics/tasks/event_type_dist.py
@@ -2,9 +2,6 @@
 
 import logging
 
-import luigi
-import luigi.task
-
 from edx.analytics.tasks.mapreduce import MapReduceJobTaskMixin, MapReduceJobTask
 from edx.analytics.tasks.pathutil import EventLogSelectionDownstreamMixin, EventLogSelectionMixin
 
@@ -14,22 +11,20 @@ log = logging.getLogger(__name__)
 
 class EventTypeDistributionTask(EventLogSelectionMixin, MapReduceJobTask):
     """Produce a dataset which tells which type of events occured in a given time interval"""
-    output_root = luigi.Parameter()
 
     def mapper(self, line):
         value = self.get_event_and_date_string(line)
         event, _date_string = value
 
         event_type = str(event.get('event_type'))
+        event_date = str(event.get('time')).split("T")[0]
         if event_type.startswith('/'):
             #Ignore events that begin with a slash
             log.error("Invalid event type occurred")
             return
-        yield (event_type), 1
-        pass
+        yield (event_date,event_type), 1
 
     def reducer(self, key, values):
-        event_type = key
+        event_date_type = key
         event_count = sum(values)
-        yield (event_type), event_count
-        pass
+        yield (event_date_type), event_count
