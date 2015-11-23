da6e0e8d712293d09d7ee4a88ffe7efe31d82b1a
Umer Farooq <umerfarooq@UmerFarooq-MBP.local>
2015-11-23 20:06:26 +0500

    fixing path for vertica to find

--- edx/analytics/tasks/tests/test_event_type_dist.py
+++ edx/analytics/tasks/tests/test_event_type_dist.py
@@ -3,7 +3,7 @@
 import json
 
 import luigi
-
+from edx.analytics.tasks.answer_dist import ProblemCheckEventMixin
 from edx.analytics.tasks.event_type_dist import (EventTypeDistributionTask)
 
 from edx.analytics.tasks.tests import unittest
@@ -11,84 +11,69 @@ from edx.analytics.tasks.tests.map_reduce_mixins import MapperTestMixin, Reducer
 from edx.analytics.tasks.tests.opaque_key_mixins import InitializeOpaqueKeysMixin
 
 
-class EventTypeDistributionTaskMapTest(MapperTestMixin, InitializeOpaqueKeysMixin, unittest.TestCase):
+
+class EventTypeDistributionBaseTest(MapperTestMixin, ReducerTestMixin, unittest.TestCase):
+    """ Base test class for Event type distribution task """
+
+    def initialize_ids(self):
+        """Define set of id values for use in tests."""
+        raise NotImplementedError
 
     def setUp(self):
-        self.task_class = EventTypeDistributionTask
-        super(EventTypeDistributionTask, self).setUp()
+        self.task_class = ProblemCheckEventMixin
+        super(EventTypeDistributionBaseTest, self).setUp()
 
-        self.interval = '2014-01-01-2015-12-30'
-        self.src = 'hdfs://localhost:9000/data/tracking.log'
-        self.output_root = 'hdfs://localhost:9000/edx-analytics-pipeline/output'
-        self.n_reduce_tasks = 1
+        self.initialize_ids()
+        self.username = 'test_user'
+        self.user_id = 24
+        self.timestamp = "2013-12-17T15:38:32.805444"
+        self.earlier_timestamp = "2013-12-15T15:38:32.805444"
+        self.reduce_key = (self.event_date, self.event_type, self.event_source)  # pylint: disable=no-member
 
-        self.event_templates = {
-            'event': {
-                "username": "test_user",
-                "host": "test_host",
-                "event_source": "server",
-                "event_type": "edx.course.enrollment.activated",
-                "context": {
-                    "course_id": self.course_id,
-                    "org_id": self.org_id,
-                    "user_id": self.user_id,
-                },
-                "time": "{0}+00:00".format(self.timestamp),
-                "ip": "127.0.0.1",
-                "event": {
-                    "course_id": self.course_id,
-                    "user_id": self.user_id,
-                    "mode": "honor",
+class EventTypeDistributionTaskMapTest(EventTypeDistributionBaseTest, InitializeOpaqueKeysMixin):
+    """ Test if mapper works for event type distribution"""
 
-                }
-            }
-        }
-
-        self.default_event_template = 'event'
-
-        self.expected_key = (self.event_type, self.event_date)
-
-
-    def test_event_is_none(self):
-        line = '{"username": "staff", "event_source": "browser", "name": "problem_check", "accept_language": "en-US,en;q=0.8", "time": "2015-08-17T20:51:13.628738+00:00", "agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.125 Safari/537.36", "page": "http://localhost:8000/courses/course-v1:edX+DemoX+Demo_Course/courseware/interactive_demonstrations/basic_questions/", "host": "precise64", "session": "618be66ec3fbf8e9d80f664704a7c706", "referer": "http://localhost:8000/courses/course-v1:edX+DemoX+Demo_Course/courseware/interactive_demonstrations/basic_questions/", "context": {"user_id": 4, "org_id": "edX", "course_id": "course-v1:edX+DemoX+Demo_Course", "path": "/event"}, "ip": "10.0.2.2", "event": "input_a0effb954cca4759994f1ac9e9434bf4_2_1=blue&input_a0effb954cca4759994f1ac9e9434bf4_3_1=choice_0&input_a0effb954cca4759994f1ac9e9434bf4_4_1%5B%5D=choice_0&input_a0effb954cca4759994f1ac9e9434bf4_4_1%5B%5D=choice_2", "event_type": ""}'
+    def test_non_problem_check_event(self):
+        line = 'this is garbage'
         self.assert_no_map_output_for(line)
 
-    def test_event_begins_with_a_slash(self):
-        line = '{"username": "staff", "event_source": "browser", "name": "problem_check", "accept_language": "en-US,en;q=0.8", "time": "2015-08-17T20:51:13.628738+00:00", "agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.125 Safari/537.36", "page": "http://localhost:8000/courses/course-v1:edX+DemoX+Demo_Course/courseware/interactive_demonstrations/basic_questions/", "host": "precise64", "session": "618be66ec3fbf8e9d80f664704a7c706", "referer": "http://localhost:8000/courses/course-v1:edX+DemoX+Demo_Course/courseware/interactive_demonstrations/basic_questions/", "context": {"user_id": 4, "org_id": "edX", "course_id": "course-v1:edX+DemoX+Demo_Course", "path": "/event"}, "ip": "10.0.2.2", "event": "input_a0effb954cca4759994f1ac9e9434bf4_2_1=blue&input_a0effb954cca4759994f1ac9e9434bf4_3_1=choice_0&input_a0effb954cca4759994f1ac9e9434bf4_4_1%5B%5D=choice_0&input_a0effb954cca4759994f1ac9e9434bf4_4_1%5B%5D=choice_2", "event_type": "/event"}'
+    def test_unparseable_problem_check_event(self):
+        line = 'this is garbage but contains problem_check'
+        self.assert_no_map_output_for(line)
+
+    def test_event_type_should_not_begin_with_a_slash(self):
+        line = self.create_event_log_line(event_type='/event')
+        self.assert_no_map_output_for(line)
+
+    def test_event_type_should_not_be_none(self):
+        line = self.create_event_log_line(event_type=None)
         self.assert_no_map_output_for(line)
 
 
-class CourseEnrollmentTaskReducerTest(ReducerTestMixin, unittest.TestCase):
+
+
+class CourseEnrollmentTaskReducerTest(EventTypeDistributionBaseTest, InitializeOpaqueKeysMixin):
     """
     Tests to verify that events-per-day-per-type reducer works correctly.
     """
-    def setUp(self):
-        self.task_class = EventTypeDistributionTask
-        super(CourseEnrollmentTaskReducerTest, self).setUp()
-        # Create the task locally, since we only need to check certain attributes
-        self.create_task()
-        self.event_type = 'problem_check'
-        self.event_date = '2015-08-17'
-        self.reduce_key = (self.event_date, self.event_type)
 
     def test_no_events(self):
         self.assert_no_output([])
 
-    def test_single_event(self):
-        inputs = [(('2013-01-01T00:00:01', "problem_check"), 1), ]
-        expected = (('2013-01-01', "problem_check", 1),)
-        self._check_output_complete_tuple(inputs, expected)
-
-    def test_multiple_events(self):
-        inputs = [
-            (('2013-01-01T00:00:01', "problem_check","chrome"), 1),
-            (('2013-01-01T00:00:01', "problem_check","chrome"), 1),
-            (('2013-02-01T00:00:01', "problem_check","safari"), 1),
-            (('2013-02-01T00:00:01', "dummy_event","chrome"), 1),
-            (('2013-03-01T00:00:01', "dummy_event_2","mozilla"), 1),
-        ]
-        expected = (('2013-01-01T00:00:01', "problem_check","chrome", 2),
-                    ('2013-02-01T00:00:01', "problem_check","safari", 1),
-                    ('2013-02-01T00:00:01', "dummy_event","chrome", 1),
-                    ('2013-03-01T00:00:01', "dummy_event_2","mozilla", 1),)
-        self._check_output_complete_tuple(inputs, expected)
+    def test_output(self,inputs,expected):
+        """
+        Args:
+            inputs:array of values to pass to reducer
+                for hard-coded key.
+            expected: dict of expected answer data
+        """
+        reducer_output = self._get_reducer_output(inputs)
+        for key, value in reducer_output:
+            event_date, event_type, event_source = key
+            count = value
+            self.assertEquals(event_date, self.event_date)
+            self.assertEquals(event_type, self.event_type)
+            self.assertEquals(event_source, self.event_source)
+            expected_key = "{}_{}".format(event_date, event_type, event_source)
+            self.assertTrue(expected_key in expected)
+            self.assertEquals(count, expected.get(expected_key))
