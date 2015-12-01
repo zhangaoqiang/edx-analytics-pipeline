0b4045cef7cf25f1a886baa516a51abb16614749
Umer Farooq <umerfarooq@UmerFarooq-MBP.local>
2015-12-01 13:26:01 +0500

    debugging mapping in event_category issue take: 3

--- edx/analytics/tasks/event_type_dist.py
+++ edx/analytics/tasks/event_type_dist.py
@@ -48,6 +48,8 @@ class EventTypeDistributionTask(EventLogSelectionMixin, MapReduceJobTask):
         if event_type.startswith('/'):
             # Ignore events that begin with a slash
             return
+        log.debug("======= reduce key ==========")
+        log.debug((event_source,event_type))
         if (event_source,event_type) in self.known_events.keys():
             event_category = self.known_events[(event_source,event_type)]
         else:
